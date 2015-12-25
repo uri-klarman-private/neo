717e21fe7ad0c0b30267cb6396458058ed6aa973
uriklarman <uri.klarman@gmail.com>
2015-12-25 04:24:00 -0600

    unifying duplicated "open socket" and PORT numbers

--- MU_MIMO/testbed/server.py
+++ MU_MIMO/testbed/server.py
@@ -5,28 +5,9 @@ import sys
 import os
 from time import sleep
 
+from MU_MIMO.testbed.client import create_socket_for_local_ip, SERVER_PORT
+
 __author__ = 'uriklarman'
-
-
-CLIENT_PORT = 1609
-BROADCASTER_PORT = 1610
-SERVER_PORT = 1611
-
-
-def create_socket_for_local_ip(port, local_ip=None):
-	if not local_ip:
-		# local_ip = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
-		local_ip = ''
-	sock = socket.socket(AF_INET, SOCK_DGRAM)
-	try:
-		sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-		sock.bind((local_ip, port))
-		sock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-		return sock
-	except:
-			print 'closing socket ', sock.getsockname()
-			sock.close()
-			raise
 
 
 def run_server(num_clients=10, server_ip='10.49.32.127', port=SERVER_PORT):
